cf4f6a894c2934d385d8851f1b665b833b9bf017
Shobhakar Tiwari <shobhakartiwari@Shobhakars-Air.lan>
2017-08-26 23:46:52 +0530

    Toast message added on Signup button

--- SignUpDemo/app/src/main/java/com/google/hagout/signupdemo/SignUp.java
+++ SignUpDemo/app/src/main/java/com/google/hagout/signupdemo/SignUp.java
@@ -1,7 +1,10 @@
 package com.google.hagout.signupdemo;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        final Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                System.out.println("on clicked");
+                Toast.makeText(getApplicationContext(), "Please enter Username", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }

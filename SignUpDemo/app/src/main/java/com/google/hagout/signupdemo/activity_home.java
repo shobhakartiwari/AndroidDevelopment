f756b974feb0bb18b0d4159bbb3c7cce3312b94a
Shobhakar Tiwari <shobhakartiwari@Shobhakars-Air.lan>
2017-08-28 17:16:20 +0530

    Hamburger menu added with functionality

--- SignUpDemo/app/src/main/java/com/google/hagout/signupdemo/activity_home.java
+++ SignUpDemo/app/src/main/java/com/google/hagout/signupdemo/activity_home.java
@@ -1,13 +1,31 @@
 package com.google.hagout.signupdemo;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class activity_home extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
